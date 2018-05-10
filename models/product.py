ffe918ddf369ea08db046a5242df42d1531fbf7a
ADMONERP\GFLORES <gflores@requiez.com>
2018-05-10 12:13:13 -0500

    nuevo_commit
    actualizacion 10-05-18

--- models/product.py
+++ models/product.py
@@ -1,7 +1,7 @@
 from openerp import api, fields, models
 
 class Product(models.Model):
-    _inherit = 'product'
-
+	nombre = fields.Char('Nombre', required=True)
 	state = fields.Boolean('State', default=True)
 	notes = fields.Char('Notes')
+	
